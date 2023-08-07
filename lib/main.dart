47af67d743bc499046855351077a9316aa1a4373
sinhasudhanshu <108268913+sinhasudhanshu@users.noreply.github.com>
2023-08-07 17:21:53 +0530

    Center Text

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(MaterialApp(home:Text("Sachin Paisa Mangta h ")));
+  runApp(MaterialApp(home:Center(child:Text("Sachin Paisa Mangta h "))));
 }
 
