37b61bc594a29f0d38a60a1092d05b8b3c95dea8
sinhasudhanshu <108268913+sinhasudhanshu@users.noreply.github.com>
2023-08-07 19:26:38 +0530

    Created Children and Child in this program

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,44 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(FirstApp());
+}
 
+class FirstApp extends StatelessWidget {
+  const FirstApp({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("AppBar"),
+
+      ),
+      body: Column(
+        children: [
+          const Text("ayush"),
+          ElevatedButton(onPressed:(){
+            print("Button");
+    }, child: const Text("button"))
+        ],
+      ),
+
+    );
+  }
+}
