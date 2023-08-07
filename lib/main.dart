e65293fbbd7779f21f7615a62825f7826e3d9b91
sinhasudhanshu <108268913+sinhasudhanshu@users.noreply.github.com>
2023-08-07 18:43:45 +0530

    created a stateless and statefull widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,36 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MaterialApp(home:Center(child:Text("Sachin Paisa Mangta h "))));
+void main() {
+  runApp(FirstApp());
 }
 
+class FirstApp extends StatelessWidget {
+  const FirstApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("AppBar"),
+
+      ),
+      body: Text("ayush"),
+    );
+  }
+}
