1a5c539f7ee49a73d49e3f0a4d1d5bb6f06be6eb
sinhasudhanshu <108268913+sinhasudhanshu@users.noreply.github.com>
2023-08-08 16:43:03 +0530

    Adding program

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -23,22 +25,109 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  int a = 0;
+  int b = 0;
+  //TextEditingController num1 = TextEditingController();
+ // TextEditingController num2 = TextEditingController();
+  int result =0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("AppBar"),
-
-      ),
       body: Column(
-        children: [
-          const Text("ayush"),
-          ElevatedButton(onPressed:(){
-            print("Button");
-    }, child: const Text("button"))
-        ],
-      ),
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text("$result",style: TextStyle(fontSize: 30),),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child: TextField(
+                //controller: num1,
 
+                onChanged: (value) {
+
+                  a = int.parse(value);
+
+                  print(a);
+
+
+                  setState(() {});
+                },
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter first number',
+                ),
+              ),
+            ),
+
+
+            Padding(padding: EdgeInsets.symmetric(horizontal:8, vertical: 16 ),
+              child: TextField(
+               // controller: num2,
+                onChanged: (value){
+                  b = int.parse(value);
+                  print(b);
+                  setState(() {
+
+                  });
+                },
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter second number',  
+                ),
+              ),
+            ),
+            ElevatedButton(onPressed: (){
+              result = a + b ;
+              print(result);
+              setState(() {
+
+              });
+
+            }, child: Text("submit"))
+
+            
+          ]),
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
