34e3efea1a3f1bbde90ac171dd99bc37d51b9379
sinhasudhanshu <108268913+sinhasudhanshu@users.noreply.github.com>
2023-08-09 16:49:13 +0530

    provide internet permission

--- lib/main.dart
+++ lib/main.dart
@@ -1,91 +1,27 @@
-import 'dart:async';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(LoginApp());
-}
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueAccent,
+        appBar: AppBar(
 
-class LoginApp extends StatelessWidget {
-  const LoginApp({super.key});
+          centerTitle: true,
+          title:  Text("Varanasi Software Junction"),
+          backgroundColor: Colors.blueGrey,
+        ),
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MyHomePage(),
-    );
-  }
-}
+        body:
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final _keyForm = GlobalKey<FormState>();
-  TextEditingController email = TextEditingController();
-  TextEditingController password = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Login"),
-      ),
-      body: Form(
-        key: _keyForm,
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-                child: TextFormField(
-                  controller: email,
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(), hintText: 'Enter email'),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'please enter email';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-                child: TextFormField(
-                  controller: password,
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(), hintText: 'Enter email'),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your password';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    if (_keyForm.currentState!.validate()) {
-                    } else {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(content: Text('please fill input')));
-                    }
-                  },
-                  child: Text("Login"))
-            ],
-          ),
+        Center(child: Image(
+          image: NetworkImage(
+              "https://images.pexels.com/photos/5077039/pexels-photo-5077039.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"),
+        ),
         ),
       ),
-    );
-  }
+    ),
+  );
 }
