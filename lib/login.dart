2af6bf688cd8411ce0aa7fb04690edecdd4bc756
sinhasudhanshu <108268913+sinhasudhanshu@users.noreply.github.com>
2023-08-10 17:26:57 +0530

    login form

--- lib/login.dart
+++ lib/login.dart
@@ -8,7 +8,9 @@ class LoginApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: LoginPage(),
+    );
   }
 }
 
