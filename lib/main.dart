8938673747ad37d129a728c9473258e6690f123a
sinhasudhanshu <108268913+sinhasudhanshu@users.noreply.github.com>
2023-08-12 16:08:58 +0530

    DIO use

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:dio/dio.dart';
 
-
 void main() async {
   final dio = Dio();
-  final response = await dio.get('https://gist.githubusercontent.com/sinhasudhanshu/b669cf91b3c0f983174b323e7c7d088d/raw/20aa166df50b2c691d0551ac9461fae961b818fd/testdata.json');
-  print(response.data);
+  try {
+    final response = await dio.get(
+        'https://gist.githubusercontent.com/sinhasudhanshu/b669cf91b3c0f983174b323e7c7d088d/raw/20aa166df50b2c691d0551ac9461fae961b818fd/testdata.json');
+    print(response.data);
+  } catch (e) {
+    print(e);
+  }
 }
