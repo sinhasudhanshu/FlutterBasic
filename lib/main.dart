fe73da5825767ae40f2b7e487afaa5d5963aa9e3
sinhasudhanshu <108268913+sinhasudhanshu@users.noreply.github.com>
2023-08-16 13:54:55 +0530

    Retrofit update

--- lib/main.dart
+++ lib/main.dart
@@ -1,148 +1,68 @@
-import 'dart:convert' as convert;
-import 'package:flutter/cupertino.dart';
+import 'package:dio/dio.dart';
+import 'package:firstproject/models/post_model.dart';
+import 'package:firstproject/service/api_service.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 
-void main() async {
-  runApp(WeatherApi());
-}
-
-class WeatherApi extends StatelessWidget {
-  const WeatherApi({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: WeatherPage(
-        title: 'Weather App',
-      ),
-    );
-  }
-}
-
-class WeatherPage extends StatefulWidget {
-  final String title;
-
-  const WeatherPage({Key? key, required this.title}) : super(key: key);
-
-  @override
-  State<WeatherPage> createState() => _WeatherPageState();
-}
-
-class _WeatherPageState extends State<WeatherPage> {
-  bool weatherFound = true;
-  String currentIcon = "";
-  String currentCityTemp = "-";
-  String currentMaxTemp = "-";
-  String currentMinTemp = "-";
-  String description = "-";
-  String cityName = "Varanasi";
-  TextEditingController _controller = TextEditingController(text: "Varanasi");
-
-  Future<void> getWeather(String cityname) async {
-    String apiKey = "4a1f8a61b74546825af1e0be106e797b";
-    final url = Uri.https("api.openweathermap.org", "/data/2.5/forecast",
-        {'q': cityname, 'appid': apiKey, 'units': 'metric'});
-    print(url);
-    try {
-      final response = await http.get(url);
-      final jsonResponse = convert.jsonDecode(response.body);
-      // print(jsonResponse);
-
-      currentIcon = jsonResponse["list"][0]["weather"][0]["icon"].toString();
-      currentCityTemp = jsonResponse["list"][0]["main"]["temp"].toString();
-      currentMaxTemp = jsonResponse["list"][0]["main"]["temp_max"].toString();
-      currentMinTemp = jsonResponse["list"][0]["main"]["temp_min"].toString();
-      description =
-          jsonResponse["list"][0]["weather"][0]["description"].toString();
-      weatherFound = true;
-      // print(currentCityTemp);
-    } catch (ex) {
-      weatherFound = false;
-      currentCityTemp = "-";
-      currentMaxTemp = "-";
-      currentMinTemp = "-";
-      description = "-";
-    }
-    setState(() {});
-  }
+class Home extends StatelessWidget {
+  const Home({Key? key}):super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("WeatherApi"),
+        backgroundColor: const Color(0xff1D1E22),
+        title: Text('Retrofit'),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: _controller,
-              onChanged: (text) {
-                cityName = text;
-              },
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Enter the city name',
+      body: _body(),
+    );
+  }
+  FutureBuilder _body() {
+    final apiService = ApiService(Dio(BaseOptions(contentType: "application/json")));
+    return FutureBuilder(
+      future: apiService.getPosts(),
+      builder: (context,snapshot){
+        if (snapshot.connectionState == ConnectionState.done){
+          final List<PostModel> posts = snapshot.data!;
+          return _posts(posts);
+        } else {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
+  }
+  Widget _posts(List<PostModel> posts){
+    return ListView.builder(
+      itemCount: posts.length,
+      itemBuilder: (context,index){
+        return Container(
+          margin: EdgeInsets.all(12),
+          padding: EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(color: Colors.black38,width: 1),
+          ),
+          child: Column(
+            children: [
+              Text(
+                posts[index].title,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+
+                ),
               ),
-            ),
+              SizedBox(height: 10,),
+              Text(
+                posts[index].title
+              )
+            ],
           ),
-          ElevatedButton(
-            child: const Text("Get Weather"),
-            onPressed: () async {
-              print("button press");
-              await getWeather(cityName);
-              setState(() {});
-            },
-          ),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  "Current Temp",
-                ),
-                Text(currentCityTemp),
-              ],
-            ),
-          ),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  "Max Temp",
-                ),
-                Text(currentMaxTemp),
-              ],
-            ),
-          ),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  "Min Temp",
-                ),
-                Text(currentMinTemp),
-              ],
-            ),
-          ),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  "Description",
-                ),
-                Text(description),
-              ],
-            ),
-          ),
-        ],
-      ),
+
+        );
+      },
     );
   }
 }
